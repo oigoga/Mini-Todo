536d34e18b046a8bc992b27249f90b3b85b39db6
oigoga <gogaelisabeth21@gmail.com>
2023-01-29 17:31:59 +0100

    added a date and time feature

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let todoForm = document.getElementById("form");
 let listOfTodo = document.querySelector("#todo__list");
 let inputField = document.querySelector("#todo__input");
-
-
+let inputFieldDate = document.querySelector("#todo__date");
+let inputFieldTime = document.querySelector("#todo__time");
+let list = document.querySelector('ul');
+let items = list.querySelectorAll('li');
 
 function deleteTodo(eventObject) {
   const buttonClicked = eventObject.target;
@@ -10,55 +12,75 @@ function deleteTodo(eventObject) {
   todoItem.remove();
 }
 
+items = Array.from(items).sort(function(a, b) {
+  let dateA = new Date(a.querySelector('.todo-date').textContent + ' ' + a.querySelector('.todo-time').textContent);
+  let dateB = new Date(b.querySelector('.todo-date').textContent + ' ' + b.querySelector('.todo-time').textContent);
+  return dateA - dateB;
+});
 
- function editTodoText (eventObject){
-  
-  let editButtonClicked = eventObject.target
-let li = editButtonClicked.parentNode.parentNode.parentNode
-let deletebtn = li.childNodes[1].childNodes[4]
-let child = li.childNodes[1].childNodes[3].innerHTML
-inputField.value = child
-let newbtn = document.createElement("button")
-newbtn.innerHTML = `Done`
-newbtn.classList.add("done-btn")
-li.append(newbtn)
-editButtonClicked.disabled = true
-newbtn.addEventListener("click", doThis)
-function doThis(){
-let childsy = li.childNodes[1].childNodes[3]
-childsy.innerHTML = inputField.value
-console.log(childsy.innerHTML)
-inputField.value = ""
-editButtonClicked.disabled = false
-newbtn.remove()
+list.innerHTML = '';
 
+for (let item of items) {
+  list.appendChild(item);
 }
-}
+
+function editTodo(eventObject) {
+  let editButtonClicked = eventObject.target;
+  let li = editButtonClicked.parentNode.parentNode.parentNode;
   
-function selectButtons(){
-  let collectButtons = document.querySelectorAll(".delete-btn")
-  let lastButton = collectButtons[collectButtons.length - 1]
-  
-  lastButton.addEventListener("click", deleteTodo)
-  
-  let collectEditButtons = document.querySelectorAll(".edit-btn")
-  let lastEditButton = collectEditButtons[collectButtons.length - 1]
-  
-  lastEditButton.addEventListener("click", editTodoText)
-  
-  
+  let child = li.childNodes[1].childNodes[3].innerHTML;
+  let childDate = li.childNodes[1].childNodes[4].innerHTML;
+  let childTime = li.childNodes[1].childNodes[5].innerHTML;
+  inputField.value = child;
+  inputFieldDate.value = childDate;
+  inputFieldTime.value = childTime;
+  let newbtn = document.createElement("button");
+  newbtn.innerHTML = `Done`;
+  newbtn.classList.add("done-btn");
+  li.append(newbtn);
+  editButtonClicked.disabled = true;
+  newbtn.addEventListener("click", doThis);
+  function doThis() {
+    let childsy = li.childNodes[1].childNodes[3];
+    let childsyDate = li.childNodes[1].childNodes[4];
+    let childsyTime = li.childNodes[1].childNodes[5];
+
+    childsy.innerHTML = inputField.value;
+    childsyDate.innerHTML = inputFieldDate.value;
+    childsyTime.innerHTML = inputFieldTime.value;
+
+    console.log(childsy.innerHTML);
+    inputField.value = "";
+    inputFieldDate.value = "";
+    inputFieldTime.value = "";
+    editButtonClicked.disabled = false;
+    newbtn.remove();
   }
-// 
+}
 
-function showTodoOnTheScreen(todo) {
- 
+function selectButtons() {
+  let collectButtons = document.querySelectorAll(".delete-btn");
+  let lastButton = collectButtons[collectButtons.length - 1];
+
+  lastButton.addEventListener("click", deleteTodo);
+
+  let collectEditButtons = document.querySelectorAll(".edit-btn");
+  let lastEditButton = collectEditButtons[collectButtons.length - 1];
+
+  lastEditButton.addEventListener("click", editTodo);
+}
+//
+
+function showTodoOnTheScreen(todo, date, time) {
   const li = document.createElement("li");
   li.classList.add("todo__section-todoItem");
 
   li.innerHTML = `
   <div class= "div-main-wrapper">
       <input class="todo-checkbox" type="checkbox" />
-      <span class="todo-text"> ${todo} </span>
+      <span class="todo-text"> ${todo}</span>
+      <span class="todo-date"> ${date}</span>
+      <span class="todo-time"> ${time}</span>
       <div class="todo_actions-wrapper">
           <button id="edit_btn" class="todo_actions edit-btn">edit</button>
           <button id="delete_btn" class="todo_actions delete-btn">delete</button>
@@ -66,27 +88,23 @@ function showTodoOnTheScreen(todo) {
   </div>
   `;
 
-
-
   listOfTodo.appendChild(li);
   selectButtons();
 }
 
 function todoFormSumbitHandler(eventObject) {
   eventObject.preventDefault();
-  
-  const todoValue = inputField.value;
-  showTodoOnTheScreen(todoValue);
+
+  const todoTextValue = inputField.value;
+  const todoDateValue = inputFieldDate.value;
+  const todoTimeValue = inputFieldTime.value;
+  showTodoOnTheScreen(todoTextValue, todoDateValue, todoTimeValue);
   inputField.value = "";
 }
 
 function clearForm() {
-
-  inputField.value = ""; 
+  inputField.value = "";
 }
-
-
-
 
 todoForm.addEventListener("submit", todoFormSumbitHandler);
 
